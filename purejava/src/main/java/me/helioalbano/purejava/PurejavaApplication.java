5c7abbb580d089883df851afc85e39044da6d684
Helio Albano de Oliveira <helioalb@gmail.com>
2021-01-26 08:11:53 -0300

    Optional examples
    Source: https://stackify.com/optional-java/

---
+++ purejava/src/main/java/me/helioalbano/purejava/PurejavaApplication.java
@@ -0,0 +1,13 @@
+package me.helioalbano.purejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurejavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurejavaApplication.class, args);
+	}
+
+}
---
+++ purejava/src/test/java/me/helioalbano/purejava/PurejavaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.helioalbano.purejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurejavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
